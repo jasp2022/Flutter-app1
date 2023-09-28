a8e7c31565949fddb24ed8e86ab956f67097405c
JS <jesusilpl@gmail.com>
2023-09-28 17:40:35 -0500

    app1-flutter

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_application_app1
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_app1/screens/search_screen.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+const MyApp({Key? key}) : super(key: key);
+
+@override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:HomePage(),
+    );  
+  }
+}
+
+class HomePage extends StatelessWidget{
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+          appBar: AppBar(
+            title: const Text("Mi Primer Proyecto"),
+            actions: [
+              IconButton(
+                onPressed: (){
+                  Navigator.push
+                  (context, 
+                  MaterialPageRoute(builder: (context) => const SearchScreen()));
+                },
+                icon: const Icon(Icons.search),
+              ),
+            ],
+          ),
+         body: Center(
+
+
+        
+
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'OFERTA DE FORMACIÓN',
+              style: TextStyle(fontSize: 30, color: Colors.blue, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20,),
+              Image.network('https://www.sena.edu.co/es-co/Noticias/PublishingImages/Foto_1%20(7).jpg', width: 200, height: 200,),
+              const SizedBox(height: 20,),
+              const Text(
+                '¡Bienvenido a mi primer proyecto!',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+
+          ]
+          ),
+          ),
+
+
+
+
+      );
+  }
+}
